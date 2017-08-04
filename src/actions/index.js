82725ded4583db69ae5dadd50a1242f687a9a825
Logan Truong <ttluan411@gmail.com>
2017-08-04 12:20:46 -0600

    second commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,7 @@ const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${AP
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`;
-    const request = axios.get(url)
-
+    const request = axios.get(url);
     
     return {
         type: FETCH_WEATHER,
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
     switch(action.type) {
         case FETCH_WEATHER:
-        return [action.payload.data];
+        return [action.payload.data,...state];
     }
     return state;
 }
